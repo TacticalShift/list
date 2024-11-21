45c273aef2e73c74dc0519aeb8bde65d7f26f70f
10Dozen <steel.mjr@gmail.com>
2024-11-21 18:42:29 +0300

    Update Media.js

--- Media.js
+++ Media.js
@@ -1,10 +1,202 @@
 const MISSION_MEDIA = [
 	{
 		"mission": "assault on myro",
-		"link": "https://www.youtube.com/embed/_SBSAgW7Ftw",
+		"link": "https://www.youtube.com/embed/_SBSAgW7Ftw"
 	},
 	{
 		"mission": "assault on myro",
-		"link": "https://www.youtube.com/embed/GLTfFFklkLY",
+		"link": "https://www.youtube.com/embed/GLTfFFklkLY"
+	},
+	{
+		"mission": "assault on myro",
+		"link": "https://www.youtube.com/embed/S61At3rBxd4"
+	},
+	{
+		"mission": "fix and flank",
+		"link": "https://www.youtube.com/embed/oVRfv8QfgK0"
+	},
+	{
+		"mission": "loco express",
+		"link": "https://www.youtube.com/embed/Hj422IZU2LE"
+	},
+	{
+		"mission": "highland",
+		"link": "https://www.youtube.com/embed/Sl60Bco9gQc"
+	},
+	{
+		"mission": "steel beasts",
+		"link": "https://www.youtube.com/embed/Soyi9qNu-xA"
+	},
+	{
+		"mission": "fb p1 bridge",
+		"link": "https://www.youtube.com/embed/JeI2WDToI04"
+	},
+	{
+		"mission": "snake hunters",
+		"link": "https://www.youtube.com/embed/_4WuXJjgOmU"
+	},
+	{
+		"mission": "snake hunters",
+		"link": "https://www.youtube.com/embed/JaRAzMmDd74"
+	},
+	{
+		"mission": "operation redemptio",
+		"link": "https://www.youtube.com/embed/BHLf2M_DwFI"
+	},
+	{
+		"mission": "sowing path",
+		"link": "https://www.youtube.com/embed/Hdo926_fQBQ"
+	},
+	{
+		"mission": "angel slayer",
+		"link": "https://www.youtube.com/embed/fTMcKEtGHvA"
+	},
+	{
+		"mission": "chernogorsk in fire",
+		"link": "https://www.youtube.com/embed/zHlVWnqHRMQ"
+	},
+	{
+		"mission": "metalurg",
+		"link": "https://www.youtube.com/embed/0M3z_fRJ16o"
+	},
+	{
+		"mission": "mechanized assault",
+		"link": "https://www.youtube.com/embed/_KETXAGlMLE"
+	},
+	{
+		"mission": "road to kamensk",
+		"link": "https://www.youtube.com/embed/OewKAlkCyKE"
+	},
+	{
+		"mission": "superior technology",
+		"link": "https://www.youtube.com/embed/3jClnYLxwnQ"
+	},
+	{
+		"mission": "crimson action",
+		"link": "https://www.youtube.com/embed/EznSaSOmIRg"
+	},
+	{
+		"mission": "beheaded",
+		"link": "https://www.youtube.com/embed/6m34NlmDSgU"
+	},
+	{
+		"mission": "black gold",
+		"link": "https://www.youtube.com/embed/6gpPieXNldA"
+	},
+	{
+		"mission": "black gold",
+		"link": "https://www.youtube.com/embed/4tn8lZ6qKGs"
+	},
+	{
+		"mission": "rokossowski",
+		"link": "https://www.youtube.com/embed/im4qMS57X9c"
+	},
+	{
+		"mission": "treelines",
+		"link": "https://www.youtube.com/embed/2U7ELO66FNw"
+	},
+	{
+		"mission": "deadly column",
+		"link": "https://www.youtube.com/embed/G1ZLA-J1qII"
+	},
+	{
+		"mission": "striped_guard",
+		"link": "https://www.youtube.com/embed/DPPbiqI8k0s"
+	},
+	{
+		"mission": "operation edelweiss",
+		"link": "https://www.youtube.com/embed/eLVmOQFld0k"
+	},
+	{
+		"mission": "intet nytt",
+		"link": "https://www.youtube.com/embed/cUiK3NAV1-U"
+	},
+	{
+		"mission": "blue try",
+		"link": "https://www.youtube.com/embed/q7iTctDG0_k"
+	},
+	{
+		"mission": "special activity",
+		"link": "https://www.youtube.com/embed/khOX6B55t2Q"
+	},
+	{
+		"mission": "snow storm",
+		"link": "https://www.youtube.com/embed/zxDG33xTzao"
+	},
+	{
+		"mission": "sea storm",
+		"link": "https://www.youtube.com/embed/8QkmHdRPLVM"
+	},
+	{
+		"mission": "dobré záměry",
+		"link": "https://www.youtube.com/embed/dq7X7tIJCI4"
+	},
+	{
+		"mission": "operation aljaraniat",
+		"link": "https://www.youtube.com/embed/KZsYibDLasE"
+	},
+	{
+		"mission": "desperate business",
+		"link": "https://www.youtube.com/embed/OhP1vZ5uSsI"
+	},
+	{
+		"mission": "leopard falling",
+		"link": "https://www.youtube.com/embed/kB3AvbZWbsU"
+	},
+	{
+		"mission": "break line",
+		"link": "https://www.youtube.com/embed/zGPpR562Rs0"
+	},
+	{
+		"mission": "lost and found",
+		"link": "https://www.youtube.com/embed/reR_eQLdk7U"
+	},
+	{
+		"mission": "death standing",
+		"link": "https://www.youtube.com/embed/kcaoA4k9z2E"
+	},
+	{
+		"mission": "the wild hunt",
+		"link": "https://www.youtube.com/embed/TBl7T3ygUto"
+	},
+	{
+		"mission": "rush to kuweiris",
+		"link": "https://www.youtube.com/embed/Ac7BfmFOfWI"
+	},
+	{
+		"mission": "save korobochka",
+		"link": "https://www.youtube.com/embed/dVlYU8tXXYw"
+	},
+	{
+		"mission": "before the storm",
+		"link": "https://www.youtube.com/embed/2_QJjhdcFLE"
+	},
+	{
+		"mission": "ghost recon: eager smoke",
+		"link": "https://www.youtube.com/embed/IacQ-1Phbbc"
+	},
+	{
+		"mission": "route_27_2",
+		"link": "https://www.youtube.com/embed/IZA1v5IlWuk"
+	},
+	{
+		"mission": "cia radarworks",
+		"link": "https://www.youtube.com/embed/dK9jojNIkOc"
+	},
+	{
+		"mission": "ghost recon - iron dragon",
+		"link": "https://www.youtube.com/embed/gCmymmWUjHc"
+	},
+	{
+		"mission": "tora! tora! tora!",
+		"link": "https://www.youtube.com/embed/6Ut1gIpWnOs"
+	},
+	{
+		"mission": "unpleasant situation",
+		"link": "https://www.youtube.com/embed/Ye4yh7DE3ts"
+	},
+	{
+		"mission": "belfast_patrol",
+		"link": "https://www.youtube.com/embed/IHRo98NWQkI"
 	}
 ]
